b23f3bc5c84610df62933686914a7cb3e4bca4bd
Andrey Larin <larin.projects@gmail.com>
2017-08-21 00:47:04 +0000

    Module 2.3. Added first models and migrations

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='likes')
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.added_at <= now
+
+    def popular(self):
+        return self.all().order_by('-rating')
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
---
+++ create-db.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mysql -uroot -e "CREATE DATABASE ask CHARACTER SET utf8 COLLATE utf8_general_ci;"
