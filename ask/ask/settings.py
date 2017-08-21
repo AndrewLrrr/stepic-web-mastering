36b68af07b2f67d1010ed75885e274c3f59a762f
Andrey Larin <larin.projects@gmail.com>
2017-08-21 23:37:01 +0000

    Module 2.5. Removed extra settings

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,8 +25,6 @@ SECRET_KEY = '%i=*=&s1)@@hj#6h+0@23eq!*_-)!(!)1izn4xt@-e$g@5x-ge'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 
